5d52fd203c2095670bc6bab7b14ed974d2a6f2eb
Asad Ahmad <aa18514@ic.ac.uk>
2017-01-26 17:27:12 +0000

    first executable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from machine import Pin, I2C
 import time
 
-if __name__ == "main"
-i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 500000)
-available_address = i2c.scan(); 
-number_of_bytes = 4; 
-while True:
-	print(i2c.readfrom(available_address[0], number_of_bytes))
-	time.sleep(5)
+if __name__ == "main":
+	i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 500000)
+	available_address = i2c.scan(); 
+	number_of_bytes = 4; 
+	while True:
+		print(i2c.readfrom(available_address[0], number_of_bytes))
+		time.sleep(5)
