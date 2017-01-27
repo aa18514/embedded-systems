ce0a306067310ed3c3dba1008ce907efe5dca971
Asad Ahmad <aa18514@ic.ac.uk>
2017-01-27 00:51:25 +0000

    imported binascii module

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from machine import Pin, I2C
-import time
+import utime
+import ubinascii
 
 if __name__ == "main":
 	i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 500000)
 	available_address = i2c.scan(); 
 	number_of_bytes = 4; 
+	data = ubinascii.hexlify(i2c.readfrom(available_address[0], number_of_bytes))
 	while True:
-		print(i2c.readfrom(available_address[0], number_of_bytes))
-		time.sleep(5)
+		print(str(data))
+		utime.sleep(5)
+		
