8f43324ec212b5df9069d5f5491d1c963e1e39a4
aa18514 <aa18514@ic.ac.uk>
2017-01-27 19:05:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from machine import Pin, I2C
 import utime
-import ubinascii
+import ubinascii import hexlify
 
 if __name__ == "main":
 	i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 500000)
-	available_address = i2c.scan(); 
+	slave_address = i2c.scan(); 
 	number_of_bytes = 4; 
-	data = ubinascii.hexlify(i2c.readfrom(available_address[0], number_of_bytes))
 	while True:
+		for i in range(0, 13):
+			data[i] = hexlify(i2c.readfrom_mem(slave_address[0], i, number_of_bytes)) #read the values of all the available registers and store them
 		print(str(data))
 		utime.sleep(5)
 		
