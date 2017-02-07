5c9150377b5c1b8ee2ee184e25c4fe9a1abf83e1
Asad Ahmad <aa18514@ic.ac.uk>
2017-02-07 22:49:47 +0000

    -modified network.py, function called in main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from machine import Pin, I2C
 import utime
 import ustruct as struct
-
-from umqtt.simple import MQTTClient
-
+import network
 
 def construct(data_1, data_2):
 	return bytearray(data_1 + data_2)
@@ -17,14 +15,12 @@ def write(data, label):
 	print(label + "{}\n".format(struct.unpack('>h', data)[0]))
 
 if __name__ == "main":
-	client = MQTTClient(machine.unique_id(),"192.168.0.10")
-	client.connect()
-	client.publish("asad",bytes(data,'utf-8'))
 	i2c = I2C(scl = Pin(5), sda = Pin(4), freq = 500000)
 	slave_address = i2c.scan() 
 	number_of_bytes = 1 
 	data = [0]*13
 	i2c.writeto_mem(slave_address[0], 2, b"\x00") #enable test mode
+	network.connect()
 	while True:
 		for i in range(0, 13):
 			data[i] = i2c.readfrom_mem(slave_address[0], i, number_of_bytes) #second byte is the register address, third byte is the number of bytes read
