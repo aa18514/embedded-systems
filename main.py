35a58d63f162f6d0101ddf5424279e1c7c6d5355
aa18514 <aa18514@ic.ac.uk>
2017-02-09 22:35:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, RTC
 import utime
 import ustruct as struct
 import networks
@@ -10,31 +10,37 @@ class i2c():
 		self.__slave_address = self.__ic.scan()
 
 	def enable_test_mode(self): 
+		"""8-average, 15 default, positive self - test measurement
+		Gain = 5, Continious measurement mode"""
+		self.__ic.writeto_mem(self.__slave_address[0], 0, b"\x71")
+		self.__ic.writeto_mem(self.__slave_address[0], 1, b"\xA0")
 		self.__ic.writeto_mem(self.__slave_address[0], 2, b"\x00")
-	
-	def update_x(self, x_msb, x_lsb):
-		self.__x = struct.unpack('>h', x_msb + x_lsb)[0]
+		utime.sleep_ms(6)
 
-	def update_z(self, z_msb, z_lsb):
-		self.__z = struct.unpack('>h', z_msb + z_lsb)[0]
+	def update_x(self, data):
+		self.__x = struct.unpack('>h', data)[0]
+
+	def update_z(self, data):
+		self.__z = struct.unpack('>h', data)[0]
 	
-	def update_y(self, y_msb, y_lsb):
-		self.__y = struct.unpack('>h', y_msb + y_lsb)[0]
+	def update_y(self, data):
+		self.__y = struct.unpack('>h', data)[0]
 	
 	def update_magnitude(self): 
 		self.__magnitude = (self.__x**2 + self.__y**2 + self.__z**2)**0.5
 
 	def start_recieving_data(self): 
-		data = [0]*13
 		while True:	#should change this later 
-			for i in range(0, 13):
-				data[i] = self.__ic.readfrom_mem(self.__slave_address[0], i, 1) #second byte is the register address, third byte is the number of bytes read
-			self.update_x(data[3], data[4])
-			self.update_z(data[5], data[6])
-			self.update_y(data[7], data[8])
+			data = [0]*6
+			data = self.__ic.readfrom_mem(self.__slave_address[0], 3, 6)
+			self.update_x(data[0:2])
+			self.update_z(data[2:4])
+			self.update_y(data[4:6])
 			self.update_magnitude()
-			message = self.__magnitude
-			print(message)
+			print(self.__x)
+			print(self.__y)
+			print(self.__z)
+			self.update_magnitude()
 			utime.sleep(1)
 
 if __name__ == "main":
@@ -43,3 +49,4 @@ if __name__ == "main":
 	IC = i2c(5, 4, 50000)  
 	IC.enable_test_mode()
 	IC.start_recieving_data()
+
