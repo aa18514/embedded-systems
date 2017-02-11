deedbd67c83eda3f71a87e00b9d0f1e4ac755724
Asad Ahmad <aa18514@ic.ac.uk>
2017-02-11 17:59:29 +0000

    test x y and z coordinates working

---
+++ network.py
@@ -0,0 +1,42 @@
+import network
+import machine
+from umqtt.simple import MQTTClient
+
+def init_wlan_and_client(ip_address, id): 
+	"""initialize the wireless network
+	and set up the client with the appropiate 
+	ip_address and id"""
+	ap_if = network.WLAN(network.AP_IF)
+	ap_if.active(False)
+	sta_if =network.WLAN(network.STA_IF)
+	sta_if.active(True)
+	sta_if.connect('EEERover','exhibition')
+	while not sta_if.isconnected(): #block until the we are connected to the internet
+		pass
+	client = MQTTClient(id,ip_address)
+	return client
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+
+def publish(client, message):
+	payload = json.dumps(message)
+	print(payload)
+	client.publish(host_address, bytes(message, 'utf-8'))
+
+def connect_client(client, host_address, message):
+	client.publish(host_address ,bytes(message,'utf-8'))
+
+def connect():
+	client = init_wlan_and_client('192.168.0.10','asdid')
+	client.set_callback(sub_cb)
+	client.connect()
+	cline.subsribe(b"time")
+	huzzah = True
+	while huzzah: 
+		if True: 
+			c.wait_msg()
+		else: 
+			c.check_msg() 
+			huzzah = False
+	return client
